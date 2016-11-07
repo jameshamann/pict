7b6d3cb7636d78a2f31749b73eeecb1e28a96c3d
jameshamann <jameshamann@me.com>
2016-11-07 14:44:16 +0000

    adds comments as a resource of photos

---
+++ spec/features/adding_comments_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+feature 'comments' do
+  context 'comments don\'t exisit' do
+    let!(:photo) { Photo.create(name: "Music", description: "Cool!") }
+    scenario 'a visitor can add comments to an existing photo' do
+      visit_photo(photo)
+      click_link('Add comment')
+      expect(path).to eq "/photos/#{photo.id}/comments/new"
+      expect(page).to have_content('Add comment!')
+      fill_in 'Comment', with: "Looking good!"
+      click_button 'Create comment'
+      expect(current_path).to eq "/photos/#{photo.id}"
+      expect(page).to have_content 'Comment: Looking good!'
+
+    end
+
+  end
+
+
+end
