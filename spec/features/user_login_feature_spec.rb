046a1e655a03a9011b21a93a91d86a7626271ae5
jameshamann <jameshamann@me.com>
2016-11-09 16:09:36 +0000

    adds user_feature_login tests

---
+++ spec/features/user_login_feature_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+feature "user login" do
+
+  context "user not signed in" do
+    scenario "login and logout links are present" do
+      visit "/"
+      expect(page).to have_content("Sign up")
+      expect(page).to have_content("Sign in")
+    end
+  end
+
+  scenario "should not see 'sign out' link" do
+      visit('/')
+      expect(page).not_to have_link('Sign out')
+    end
+  end
+
+  context "user signed in on the homepage" do
+    before do
+      sign_up(email: 'test@example.com', password: '123456')
+      visit('/')
+    end
+
+    scenario "should see 'sign out' link and user email in nav bar" do
+      expect(page).to have_link('Sign out')
+      expect(page).to have_content("Signed in as: test@example.com")
+    end
+
+    scenario "should not see a 'sign in' link and a 'sign up' link" do
+      expect(page).not_to have_link('Sign in')
+      expect(page).not_to have_link('Sign up')
+    end
+  end
